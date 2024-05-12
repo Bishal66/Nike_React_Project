67e1579407d7f966783fc771da2241098dba2fe5
Bishal <bishalbhandari50319@gmail.com>
2024-05-12 23:15:06 +1000

    Set up all the files and made reusable Navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="/img/brand_logo.png" alt="logo" />
+      </div>
+
+
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+
+      </ul>
+
+      <button>Log In</button>
+    </nav>
+  )
+
+}
+
+
+export default Navigation;
